6ab82634167ae4ea9e534e3f34c73aed8b93f203
Nhlanhla <ntshabalalait@gmail.com>
2026-03-29 17:56:18 +0200

    Add webpack configuration files and update package dependencies

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,25 @@
-// webpack.config.js
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-        watchFiles: ["./src/template.html"],
+    entry: {
+        app: './src/index.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/index.html",
+            title: 'Production',
         }),
     ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
     module: {
         rules: [
             {
